547e62370cc89b74a4a9f1b3061994cdcfcbf96f
JalissaRandle <Jalissar18@Gmail.com>
2022-04-01 14:39:50 -0500

    second commit

--- process.py
+++ process.py
@@ -7,9 +7,9 @@ def sales_reports(log_file):
     for line in log_file:
         # this just created a vairable inside of log_file and the function of it is below
         line = line.rstrip()
-        #defininf what line is,and .strip means to strip the txt file of any extra wordage at the beginning and end 
+        #defininf what line is,and .strip means to strip the txt file of any extra wordage at the beginning and end  
         day = line[0:3]
-        #this is creating a new variable called day and day is = to index 0 
+        #this is creating a new variable called day and day is = to index 0  and the first 3 charaters
         if day == "Mon":
         #this is saying if the first index is equal to tues
             print(line)
